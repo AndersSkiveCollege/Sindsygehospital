245c0ec0d8f5acb5d85ffef0de1d743ca77d332d
Jackfrog1 <112097363+Jackfrog1@users.noreply.github.com>
2022-11-05 11:36:19 +0100

    worked a little on enemy ai

--- Assets/Scripts/AIEnemyFinal.cs
+++ Assets/Scripts/AIEnemyFinal.cs
@@ -9,10 +9,16 @@ public class AIEnemyFinal : MonoBehaviour
     Transform player;
     public GameObject manSkin;
     Animator anim;
+    public bool wondering;
+    public float wonderMaxStandStillTime;
+    public float wonderMinStandStillTime;
+    public float wonderMinWalkRange;
+    public float wonderMaxWalkRange;
+
 
     public enum states
     {
-        search, chase, attack, chill, walk
+        search, chase, attack, chill, wonder
     }
 
     public states myState = new states();
@@ -24,6 +30,7 @@ public class AIEnemyFinal : MonoBehaviour
         player = GameObject.Find("PlayerCapsule").transform;
         agent = GetComponent<NavMeshAgent>();
         anim = manSkin.GetComponent<Animator>();
+        myState = states.wonder;
     }
 
     
@@ -44,11 +51,16 @@ public class AIEnemyFinal : MonoBehaviour
                 break;
 
             case states.chill:
-                Attack();
+                Chill();
                 break;
 
-            case states.walk:
-                Attack();
+            case states.wonder:
+
+                if (wondering == false)
+                {
+                    StartCoroutine("Wonder");
+                }
+                
                 break;
         }
     }
@@ -73,8 +85,20 @@ public class AIEnemyFinal : MonoBehaviour
 
     }
 
-    void Walk()
+    IEnumerator Wonder()
     {
 
+        while (myState == states.wonder)
+        {
+            anim.SetInteger("Movement", 1);
+            float wonderRadius = Random.Range(wonderMinWalkRange,wonderMaxWalkRange);
+            Vector3 randomDirection = Random.insideUnitSphere * wonderRadius;
+            randomDirection += transform.position;
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomDirection, out hit, wonderRadius, 1);
+            Vector3 finalPosition = hit.position;
+            GetComponent<NavMeshAgent>().destination = finalPosition;
+            yield return new WaitForSecondsRealtime(Random.Range(wonderMinStandStillTime,wonderMaxStandStillTime));
+        }
     }
 }
